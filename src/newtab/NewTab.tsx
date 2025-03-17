aaeec1c5e7eccd0c25872164a3327dacfc15f102
yangfeng <1719957182@qq.com>
2025-03-18 02:20:00 +0800

    feat: complete the layout

--- src/newtab/NewTab.tsx
+++ src/newtab/NewTab.tsx
@@ -4,13 +4,13 @@ export default function NewTab(): JSX.Element {
     return (
         <div id='my-ext' data-theme='light'>
             <div id='main-container' className='h-screen w-screen flex bg-#FAFAFA'>
-                <div id="navigation-panel-group" className='h-full w-290 flex-1 flex'>
-                    <div id='workspace-container' className='h-full w-70 flex-1 py-16px bg-black' />
-                    <div id='space-container' className='h-full w-220 flex-1 border-x-1 border-solid border-#DDDDF5 bg-red-500' />
+                <div id="navigation-panel-group" className='h-full flex-none basis-290 flex'>
+                    <div id='workspace-container' className='h-full flex-none basis-70 py-16 bg-yellow-50' />
+                    <div id='space-container' className='h-full flex-none basis-220 border-x-1 border-solid border-#DDDDF5 bg-red-50' />
                 </div>
                 <div id="data-panel-group" className='h-full w-10 flex-auto flex'>
-                    <div id='collection-container' className='h-full grow-85 shrink border-r-1 border-solid border-#DDDDF5' />
-                    <div id='tab-container' className='h-full grow-15 shrink' />
+                    <div id='collection-container' className='h-full grow-85 shrink border-r-1 border-solid border-#DDDDF5 bg-green-50' />
+                    <div id='tab-container' className='h-full grow-15 shrink bg-blue-50' />
 
                 </div>
 
