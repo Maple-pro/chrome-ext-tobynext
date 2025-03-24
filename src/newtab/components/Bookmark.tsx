60ea5943e55f4f0c6f38f4a5a1d5c4d042897ce8
yangfeng <1719957182@qq.com>
2025-03-24 13:58:04 +0800

    feat: modify margin between bookmarks

--- src/newtab/components/Bookmark.tsx
+++ src/newtab/components/Bookmark.tsx
@@ -8,7 +8,7 @@ interface BookmarkProps {
 
 const Bookmark = (props: BookmarkProps): JSX.Element => {
     return (
-        <div id="bookmark-container" className="w-full h-40 my-5 px-20 py-10 rounded-md border-1 border-solid border-[#DDDDF5] shadow-sm shadow-[#DDDDF5] flex flex-row items-center justify-between">
+        <div id="bookmark-container" className="w-full h-40 my-10 px-20 py-10 rounded-md border-1 border-solid border-[#DDDDF5] shadow-sm shadow-[#DDDDF5] flex flex-row items-center justify-between">
             <div id="bookmark-title" className="truncate text-[14px] basis-1/2 shrink-1 grow-0 pr-20">
                 {props.bookmark.title}
             </div>
